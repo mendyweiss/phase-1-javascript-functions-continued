e342d0c4dca435953f2a30229c5914263f92989b
Mendy <mendyw94@gmail.com>
2021-12-23 09:03:36 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (activty = "roller-skate") {
+    return `This Saturday, I want to ${activty}!`;
+}
+
+function mondayWork (place ='go to the office') {
+    return `This Monday, I will ${place}.`;
+}
+
+function wrapAdjective (highlight = '*') {
+    return function /*innerFunc*/ (wordsToBeHighLighted = 'a hard worker') {
+        return `You are ${highlight}${wordsToBeHighLighted}${highlight}!`;
+    };
+}/*("*")('a hard worker')*/;
